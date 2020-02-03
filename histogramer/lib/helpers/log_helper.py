a18f8956786e1d545537ef57f3ea3c12549a5582
Peter <53820566+pkomissarov-gd@users.noreply.github.com>
2020-02-03 18:29:36 +0300

    add several tests (#13)
    * change architecture
    
    * change architecture
    
    * refactoring
    
    * add multiprocess mode for files processing
    
    * add packages for pytest
    
    * add tests

---
+++ histogramer/lib/helpers/log_helper.py
@@ -0,0 +1,38 @@
+"""
+helps to work with console & file logger
+"""
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+
+def init_logger(path, folder_name=".logs"):
+    """
+    configure logger for logging events in console (and in a file, optional)
+    :param folder_name: name of the folder where logs will be stored
+    :param path: path to the log folder
+    :return: None
+    """
+    log_formatter = logging.Formatter("[%(asctime)s] "
+                                      "[%(threadName)s] "
+                                      "[%(levelname)s] "
+                                      "%(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(fmt=log_formatter)
+    console_handler.setLevel(level=logging.ERROR)
+    logger.addHandler(hdlr=console_handler)
+
+    if path != "0":
+        path = f"{path}/{folder_name}/"
+        Path(path).mkdir(parents=True, exist_ok=True)
+        max_size = 2 * 1024 * 1024  # 2 mb
+        rotating_file_handler = RotatingFileHandler(
+            filename=f"{path}.histogramer",
+            maxBytes=max_size,
+            backupCount=4)
+        rotating_file_handler.setFormatter(fmt=log_formatter)
+        rotating_file_handler.setLevel(level=logging.INFO)
+        logger.addHandler(hdlr=rotating_file_handler)
--- histogramer/tests/__init__.py
+++ histogramer/tests/__init__.py
@@ -1,3 +1,11 @@
 """
 Init for 'tests' python package
 """
+import codecs
+
+import colorama
+
+codecs.register(
+    lambda name: codecs.lookup("utf-8") if name == "cp65001" else None)
+
+colorama.init()
